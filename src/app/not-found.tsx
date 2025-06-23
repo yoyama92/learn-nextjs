37d81293b0199eb83b0c6ad009c954ed515cbb0d
oyama <52624121+yoyama92@users.noreply.github.com>
2025-06-23 16:37:36 +0900

    404ページを追加

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+import { SignOut } from "@/components/sign-out";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-2 p-4">
+      <h2 className="text-lg font-bold">Not Found</h2>
+      <p>Could not find requested resource</p>
+      <div className="flex flex-row gap-2">
+        <div>
+          <Link href="/" className="btn btn-outline">Return Home</Link>
+        </div>
+        <SignOut />
+      </div>
+    </div>
+  );
+}
