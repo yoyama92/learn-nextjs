19c80a7853f95c3016ce32a1aee6967abe9690e5
oyama <52624121+yoyama92@users.noreply.github.com>
2025-08-15 19:26:22 +0900

    feat: vitestの設定ファイルを追加し、エイリアスを定義

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import * as path from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
